e91fb6c3b754343d6ee6cee3cb345f74eda43135
ngaivoronski <ngaivoronski@gmail.com>
2019-10-07 16:20:25 -0400

    additional board functionality

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const [homeScore, setHomeScore] = useState(0);
   const [awayScore, setAwayScore] = useState(0);
+  //const [down, setDown] = useState(1);
   return (
     <div className="container">
       <section className="scoreboard">
@@ -31,6 +32,7 @@ function App() {
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
           <button onClick={() => setHomeScore(homeScore + 7)} className="homeButtons__touchdown">Home Touchdown</button>
           <button onClick={() => setHomeScore(homeScore + 3)} className="homeButtons__fieldGoal">Home Field Goal</button>
+          
         </div>
         <div className="awayButtons">
           <button onClick={() => setAwayScore(awayScore + 7)} className="awayButtons__touchdown">Away Touchdown</button>
--- src/BottomRow.js
+++ src/BottomRow.js
@@ -1,26 +1,41 @@
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
 
+
+
 const BottomRow = () => {
+  const [down, setDown] = useState(1);
+  const [toGo, setToGo] = useState(10);
+  const [ballOn, setBallOn] = useState(0);
+  const [quarter, setQuarter] = useState(1);
   return (
+    <>
     <div className="bottomRow">
       <div className="down">
         <h3 className="down__title">Down</h3>
-        <div className="down__value">3</div>
+        <div className="down__value">{down}</div>
       </div>
       <div className="toGo">
         <h3 className="toGo__title">To Go</h3>
-        <div className="toGo__value">7</div>
+        <div className="toGo__value">{toGo}</div>
       </div>
       <div className="ballOn">
         <h3 className="ballOn__title">Ball on</h3>
-        <div className="ballOn__value">21</div>
+        <div className="ballOn__value">{ballOn}</div>
       </div>
       <div className="quarter">
         <h3 className="quarter__title">Quarter</h3>
-        <div className="quarter__value">4</div>
+        <div className="quarter__value">{quarter}</div>
       </div>
     </div>
+    <button onClick={() => setDown(down + 1)} className="homeButtons__fieldGoal">Pass</button>
+    <button onClick={() => setDown(1)} className="homeButtons__fieldGoal">Reset Down</button>
+    <button onClick={() => setToGo(toGo - 1)} className="homeButtons__fieldGoal">Less Yards to First</button>
+    <button onClick={() => setToGo(10)} className="homeButtons__fieldGoal">Reset To Go</button>
+    <button onClick={() => setBallOn(ballOn + 5)} className="homeButtons__fieldGoal">Add 5 Yards to Ball</button>
+    <button onClick={() => setBallOn(0)} className="homeButtons__fieldGoal">Reset Ball</button>
+    <button onClick={() => setQuarter(quarter + 1)} className="homeButtons__fieldGoal">Next Quarter</button>
+    </>
   );
 };
 
