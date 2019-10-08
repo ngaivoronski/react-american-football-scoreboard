9e74b4963f4c30dd37f921e84925460221818169
ngaivoronski <ngaivoronski@gmail.com>
2019-10-07 21:41:37 -0400

    remade bottom row into a class and added timer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 //TODO: STEP 1 - Import the useState hook.
-import React, {useState} from "react";
+import React, {useState, useEffect, useRef} from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
 
@@ -7,25 +7,50 @@ function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const [homeScore, setHomeScore] = useState(0);
   const [awayScore, setAwayScore] = useState(0);
-  //const [down, setDown] = useState(1);
+  const [down, setDown] = useState(1);
+  const [toGo, setToGo] = useState(10);
+  const [ballOn, setBallOn] = useState(0);
+  const [quarter, setQuarter] = useState(1);
+  const team1 = "Tigers";
+  const team2 = "Lions";
+  //const team1 = prompt('Please enter the name of Team 1');
+  //const team2 = prompt('Please enter the name of Team 2');
+  const [time, setTime] = useState(600);
+
+function timer() {
+  setTimeout(function() {
+    setTime(time - 1);
+  }, 1000);
+  var minutes = Math.floor(time/60);
+  if (minutes < 10) {
+    minutes = '0'+minutes;
+  }
+  var seconds = Math.round(time%60);
+  if (seconds < 10) {
+    seconds = '0'+seconds;
+  }
+  var clock = minutes + ":" + seconds;
+  return clock;
+}
+
   return (
     <div className="container">
       <section className="scoreboard">
         <div className="topRow">
           <div className="home">
-            <h2 className="home__name">Lions</h2>
+            <h2 className="home__name">{team1}</h2>
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
 
             <div className="home__score">{homeScore}</div>
           </div>
-          <div className="timer">00:03</div>
+          <div className="timer">{timer()}</div>
           <div className="away">
-            <h2 className="away__name">Tigers</h2>
+            <h2 className="away__name">{team2}</h2>
             <div className="away__score">{awayScore}</div>
           </div>
         </div>
-        <BottomRow />
+        <BottomRow down={down} toGo = {toGo} ballOn={ballOn} quarter={quarter}/>
       </section>
       <section className="buttons">
         <div className="homeButtons">
@@ -39,6 +64,16 @@ function App() {
           <button onClick={() => setAwayScore(awayScore + 3)} className="awayButtons__fieldGoal">Away Field Goal</button>
         </div>
       </section>
+      <section className="buttons">
+        <button onClick={() => setDown(down + 1)} className="homeButtons__fieldGoal">Pass</button>
+        <button onClick={() => setDown(1)} className="homeButtons__fieldGoal">Reset Down</button>
+        <button onClick={() => setToGo(toGo - 1)} className="homeButtons__fieldGoal">Less Yards to First</button>
+        <button onClick={() => setToGo(10)} className="homeButtons__fieldGoal">Reset To Go</button>
+        <button onClick={() => setBallOn(ballOn + 5)} className="homeButtons__fieldGoal">Add 5 Yards to Ball</button>
+        <button onClick={() => setBallOn(0)} className="homeButtons__fieldGoal">Reset Ball</button>
+        <button onClick={() => setQuarter(quarter + 1)} className="homeButtons__fieldGoal">Next Quarter</button>
+      </section>
+      
     </div>
   );
 }
--- src/BottomRow.js
+++ src/BottomRow.js
@@ -3,40 +3,29 @@ import "./App.css";
 
 
 
-const BottomRow = () => {
-  const [down, setDown] = useState(1);
-  const [toGo, setToGo] = useState(10);
-  const [ballOn, setBallOn] = useState(0);
-  const [quarter, setQuarter] = useState(1);
+class BottomRow  extends React.Component {
+  render() {
   return (
-    <>
     <div className="bottomRow">
       <div className="down">
         <h3 className="down__title">Down</h3>
-        <div className="down__value">{down}</div>
+        <div className="down__value">{this.props.down}</div>
       </div>
       <div className="toGo">
         <h3 className="toGo__title">To Go</h3>
-        <div className="toGo__value">{toGo}</div>
+        <div className="toGo__value">{this.props.toGo}</div>
       </div>
       <div className="ballOn">
         <h3 className="ballOn__title">Ball on</h3>
-        <div className="ballOn__value">{ballOn}</div>
+        <div className="ballOn__value">{this.props.ballOn}</div>
       </div>
       <div className="quarter">
         <h3 className="quarter__title">Quarter</h3>
-        <div className="quarter__value">{quarter}</div>
+        <div className="quarter__value">{this.props.quarter}</div>
       </div>
     </div>
-    <button onClick={() => setDown(down + 1)} className="homeButtons__fieldGoal">Pass</button>
-    <button onClick={() => setDown(1)} className="homeButtons__fieldGoal">Reset Down</button>
-    <button onClick={() => setToGo(toGo - 1)} className="homeButtons__fieldGoal">Less Yards to First</button>
-    <button onClick={() => setToGo(10)} className="homeButtons__fieldGoal">Reset To Go</button>
-    <button onClick={() => setBallOn(ballOn + 5)} className="homeButtons__fieldGoal">Add 5 Yards to Ball</button>
-    <button onClick={() => setBallOn(0)} className="homeButtons__fieldGoal">Reset Ball</button>
-    <button onClick={() => setQuarter(quarter + 1)} className="homeButtons__fieldGoal">Next Quarter</button>
-    </>
-  );
-};
+    );
+  };
+}
 
 export default BottomRow;
